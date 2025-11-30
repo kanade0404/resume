c476dce25ca13867650c572c9ed07960f3045ab8
kanade0404 <melty0404@gmail.com>
2025-11-30 18:49:07 +0900

    fix: allow puppeteer without sandbox

--- convert.sh
+++ convert.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e  # Exit on error
 
+LAUNCH_OPTS='{ "args": ["--no-sandbox", "--disable-setuid-sandbox"] }'
+
 for f in *.md; do
   out="${f%.md}.pdf"
   echo "Generating $out"
   md-to-pdf "$f" \
     --stylesheet ./style.css \
-    --pdf-options '{ "format": "A4", "printBackground": true }'
+    --pdf-options '{ "format": "A4", "printBackground": true }' \
+    --launch-options "$LAUNCH_OPTS"
 done
